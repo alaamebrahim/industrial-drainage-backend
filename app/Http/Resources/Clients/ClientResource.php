cd0d6681fc2d468054ef6f218909c580e7cf13eb
alaa <alaamebrahim91@gmail.com>
2024-03-10 10:31:34 +0200

    added count for claims/results

--- app/Http/Resources/Clients/ClientResource.php
+++ app/Http/Resources/Clients/ClientResource.php
@@ -22,6 +22,8 @@ class ClientResource extends JsonResource
             'total_amount' => $totalAmount = $this->claims->sum('total_amount'),
             'amount_paid' => $amountPaid = $this->payments->sum('amount'),
             'net_amount' => $totalAmount - $amountPaid,
+            'claims_count' => $this->claims()->count(),
+            'results_count' => $this->results()->count(),
         ];
     }
 }
