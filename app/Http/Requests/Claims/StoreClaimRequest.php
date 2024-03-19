fb5977e91e6301becb192626a9b5a485c0aa67a3
alaa <alaamebrahim91@gmail.com>
2024-03-19 08:55:59 +0200

    add new validation rule

--- app/Http/Requests/Claims/StoreClaimRequest.php
+++ app/Http/Requests/Claims/StoreClaimRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Claims;
 
 use App\Http\Requests\JsonFormRequest;
+use App\Models\Claim;
+use Illuminate\Validation\Validator;
 
 class StoreClaimRequest extends JsonFormRequest
 {
@@ -19,5 +21,22 @@ class StoreClaimRequest extends JsonFormRequest
             'start_date' => ['required', 'date'],
             'end_date' => ['required', 'date'],
         ];
+    }
+
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function (Validator $validator){
+            $startDateExists = Claim::query()
+                ->where('client_id', $this->get('client_id'))
+                ->whereBetween('start_date', [$this->get('start_date'), $this->get('start_date')])
+                ->exists();
+            $endDateExists = Claim::query()
+                ->where('client_id', $this->get('client_id'))
+                ->whereBetween('end_date', [$this->get('start_date'), $this->get('start_date')])
+                ->exists();
+            if ($startDateExists || $endDateExists) {
+                $validator->errors()->add('swal', 'تاريخ المطالبة غير صحيح حيث يوجد مطالبة أخرى عن نفس الفترة');
+            }
+        });
     }
 }
